1454fb4a191c20563bbce4bf34fba78f43d1ea9d
Márcio César <marcinho060498@gmail.com>
2023-12-18 21:44:53 -0300

    calculando distância quando ocorrem eventos

--- js/slide.js
+++ js/slide.js
@@ -1,38 +1,50 @@
 export default class Slide{
-    constructor(slide, wrapper){
-        this.slide = document.querySelector(slide);
-        this.wrapper = document.querySelector(wrapper);
-    }
+  constructor(slide, wrapper){
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = {finalPosition : 0, startX: 0, movement:0}
+  }
 
-    onStart(event){
-        event.preventDefault();
-        console.log('mousedown')
-        this.wrapper.addEventListener('mousemove', this.onMove);
-    }
-    
-    onMove(event){
-        console.log('moveu');
-    }
-    
-    onEnd(event){
-        console.log('acabou');
-        this.wrapper.removeEventListener('mousemove', this.onMove);
-    }
+  moveSlide(distX){
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+}
 
-    addSlideEvents(){
-        this.wrapper.addEventListener('mousedown', this.onStart);
-        this.wrapper.addEventListener('mouseup', this.onEnd);
-    }
+  updatePosition(clientX){
+    this.dist.movement = (this.dist.startX - clientX) * 1.6 ;
+    return this.dist.finalPosition - this.dist.movement;
+  }
 
-    bindEvents(){
-        this.onStart = this.onStart.bind(this);
-        this.onMove = this.onMove.bind(this);
-        this.onEnd = this.onEnd.bind(this);
-    }
+  onStart(event){
+    event.preventDefault();
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
 
-    init(){
-        this.bindEvents()
-        this.addSlideEvents();
-        return this;
-    }
+  onMove(event){
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);  
+  }
+
+  onEnd(event){
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
+  }
+
+  addSlideEvents(){
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+  }
+
+  bindEvents(){
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init(){
+    this.bindEvents()
+    this.addSlideEvents();
+    return this;
+  }
 }
